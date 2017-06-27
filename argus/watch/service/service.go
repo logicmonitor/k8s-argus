91caf75faaaf857a6bffdc6ef6dd48d2e71027bc
Andrew Rynhard <andrew@andrewrynhard.com>
2017-06-27 09:55:24 -0700

    Add option to move deleted devices to a dedicated group (#11)

--- argus/watch/service/service.go
+++ argus/watch/service/service.go
@@ -94,8 +94,32 @@ func (w Watcher) DeleteFunc() func(obj interface{}) {
 		}
 		if restResponse.Data.Total == 1 {
 			id := restResponse.Data.Items[0].Id
-			log.Infof("Deleting service %s with id %d", service.Name, id)
-			w.LMClient.DeleteDevice(id)
+			if w.Config.DeleteDevices {
+				log.Infof("Deleting service %s with id %d", service.Name, id)
+				restNullObjectResponse, _, err := w.LMClient.DeleteDevice(id)
+				if err != nil {
+					log.Printf("Failed to delete device with id %q: %s", id, restNullObjectResponse.Errmsg)
+				}
+			} else {
+				log.Infof("Moving service %s with id %d to deleted group", service.Name, id)
+				categories := constants.ServiceDeletedCategory
+				for k, v := range service.Labels {
+					categories += "," + k + "=" + v
+
+				}
+				device := lmv1.RestDevice{
+					CustomProperties: []lmv1.NameAndValue{
+						{
+							Name:  "system.categories",
+							Value: categories,
+						},
+					},
+				}
+				restResponse, _, err := w.LMClient.PatchDeviceById(device, id, "replace", "customProperties")
+				if err != nil {
+					log.Errorf("Failed to patch service %s: %s", service.Name, restResponse.Errmsg)
+				}
+			}
 		}
 	}
 }
