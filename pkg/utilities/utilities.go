0fe92c61c83c1dc0f4ff0402855674b02b7eaa2f
skt44 <30736123+skt44@users.noreply.github.com>
2018-07-30 10:38:27 -0700

    update docs (#82)
    * updating docs to add instructions for version 0.2.0-alpha.0
    
    * adding version 0.2.0-alpha.0 configuration details and removing version 0.1.0 related info
    
    * updating docs to include dynamic groups for node roles
    
    * bump version
    
    * update utilities.go

--- pkg/utilities/utilities.go
+++ pkg/utilities/utilities.go
@@ -22,7 +22,10 @@ func BuildSystemCategoriesFromLabels(categories string, labels map[string]string
 // GetLabelByPrefix takes a list of labels returns the first label matching the specified prefix
 func GetLabelByPrefix(prefix string, labels map[string]string) (string, string) {
 	for k, v := range labels {
-		if match, _ := regexp.MatchString("^"+prefix, k); match {
+		if match, err := regexp.MatchString("^"+prefix, k); match {
+			if err != nil {
+				continue
+			}
 			return k, v
 		}
 	}
