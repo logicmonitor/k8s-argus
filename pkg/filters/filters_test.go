9a9345c87cb5eff21fbbd95c1c699ae20eb5212b
Shubhashri Shirode <shubhashri.shirode@logicmonitor.com>
2020-09-09 02:46:01 -0700

    Merge pull request #61 in DEV/k8s-argus from feature/DEV-63945-discovery-filtering-processing to develop
    * commit 'a48049878a5e5e73b45a15bdbe0944f8db5c9d25':
      DEV-63945- reverted incorrect changes from lm-sdk.
      DEV-63945-review comments addressed.
      DEV-63945- review comments.
      DEV-63945- toml file changes.
      DEV-63945- added unit test cases and address review comments.
      DEV-63945- implemented update filter scenario.
      DEV-63845- fixed build failures.
      DEV-63945-logging  type modifications.
      DEV-63945-implementation of discovery filtering.

---
+++ pkg/filters/filters_test.go
@@ -0,0 +1,80 @@
+package filters
+
+import (
+	"testing"
+
+	"github.com/logicmonitor/k8s-argus/pkg/constants"
+	"github.com/stretchr/testify/assert"
+)
+
+func initFilterExprMap() {
+	expressionMap = make(map[string]string)
+	expressionMap[constants.Pods] = "l1 =~ 'v1' && name =~ 'test'"
+	expressionMap[constants.Deployments] = ""
+	expressionMap[constants.Nodes] = "*"
+	expressionMap[constants.Services] = "l2 =~ 'v*'"
+}
+
+func init() {
+	initFilterExprMap()
+}
+
+func TestEvaluate(t *testing.T) {
+	t.Parallel()
+	filterTestCases := []struct {
+		name           string
+		resource       string
+		evalParams     map[string]interface{}
+		expectedResult bool
+	}{
+		{
+			name:           "Invalid resouce name",
+			resource:       "XYZ",
+			evalParams:     getSampleEvaluationParams(),
+			expectedResult: false,
+		},
+		{
+			name:           "Pods expression, check equality",
+			resource:       constants.Pods,
+			evalParams:     getSampleEvaluationParams(),
+			expectedResult: true,
+		},
+		{
+			name:           "Nodes expression- check filter all",
+			resource:       constants.Nodes,
+			evalParams:     getSampleEvaluationParams(),
+			expectedResult: true,
+		},
+		{
+			name:           "Deployment expression- check empty",
+			resource:       constants.Deployments,
+			evalParams:     getSampleEvaluationParams(),
+			expectedResult: false,
+		},
+		{
+			name:           "Services expression- check blob expression ",
+			resource:       constants.Services,
+			evalParams:     getSampleEvaluationParams(),
+			expectedResult: true,
+		},
+	}
+
+	assert := assert.New(t)
+	// nolint: dupl
+	for _, testCase := range filterTestCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			result := Eval(testCase.resource, testCase.evalParams)
+
+			// check expected evaluation result
+			assert.Equal(testCase.expectedResult, result, "TestCase: \"%s\" \nResult: Expected evaluate \"%s\" but got \"%s\"", testCase.name, testCase.expectedResult, result)
+		})
+	}
+}
+
+func getSampleEvaluationParams() map[string]interface{} {
+	labels := make(map[string]interface{})
+	labels["l1"] = "v1"
+	labels["l2"] = "v2"
+	labels["name"] = "test-device"
+	return labels
+}
