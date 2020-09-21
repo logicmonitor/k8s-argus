b0ecc2b92164d2ba955d95eff2fcda4f561ff94d
Shubhashri Shirode <shubhashri.shirode@logicmonitor.com>
2020-09-21 06:00:22 -0700

    Merge pull request #65 in DEV/k8s-argus from feature/DEV-64223-periodic-delete to develop
    * commit '7d5deebf0bfec0c2e7d453dcabe468c51fdc13ab':
      DEV-64223- changed func name and context in logging.
      DEV-64223-implemented periodic delete for dangling resources.

--- pkg/sync/initsyncer.go
+++ pkg/sync/initsyncer.go
@@ -3,6 +3,7 @@ package sync
 import (
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/logicmonitor/k8s-argus/pkg/constants"
 	"github.com/logicmonitor/k8s-argus/pkg/device"
@@ -24,8 +25,7 @@ type InitSyncer struct {
 }
 
 // InitSync implements the initial sync through logicmonitor API
-func (i *InitSyncer) InitSync() {
-	lctx := lmlog.NewLMContextWith(logrus.WithFields(logrus.Fields{"name": "init-sync"}))
+func (i *InitSyncer) InitSync(lctx *lmctx.LMContext) {
 	log := lmlog.Logger(lctx)
 	log.Infof("Start to sync the resource devices")
 	clusterName := i.DeviceManager.Base.Config.ClusterName
@@ -195,4 +195,15 @@ func (i *InitSyncer) syncDevices(lctx *lmctx.LMContext, resourceType string, res
 			}
 		}
 	}
+}
+
+// RunPeriodicSync runs synchronization periodically.
+func (i *InitSyncer) RunPeriodicSync(syncTime int) {
+	lctx := lmlog.NewLMContextWith(logrus.WithFields(logrus.Fields{"name": "periodic-sync"}))
+	go func() {
+		for {
+			time.Sleep(time.Duration(syncTime) * time.Minute)
+			i.InitSync(lctx)
+		}
+	}()
 }
