e7b92f289d273c455be699036f322348de5803ea
vkumbhar94 <vkumbhar94@gmail.com>
2021-07-28 11:04:35 +0530

    release-6.0.0: removed pkg directory

--- pkg/mocks/doc.go
+++
@@ -1,5 +0,0 @@
-package mocks
-
-// marker to download package in vendor otherwise go generate mocks for unit testing fails with no package in vendor
-// as go modules copies only those packages those are used in code, else it cannot
-import _ "github.com/golang/mock/mockgen/model"
